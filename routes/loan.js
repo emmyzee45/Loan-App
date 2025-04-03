51744343ae0c30887d3ebf228f4df9f95444a730
Aondohemba Emmanuel <emmyaondohemba45@gmail.com>
2025-04-03 16:22:06 +0100

    loan app

--- routes/loan.js
+++ routes/loan.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { authenticate, authorize } from "../middleware/authMiddleware.js";
-import { deleteLoad, getAllLoans, getExpiredLoans, getLoansBaseOnStatus } from "../controllers/loan.js";
+import { deleteLoad, getAllLoans, getExpiredLoans, getLoansBaseOnStatus, getLoansByUser } from "../controllers/loan.js";
 
 const router = express.Router();
 
 router.get("/",authenticate, getAllLoans)
 router.post("/",authenticate, getLoansBaseOnStatus);
 router.get("/expired",authenticate, getExpiredLoans)
-router.get("/:userEmail/get",authenticate, getLoansBaseOnStatus)
+router.get("/:userEmail/get",authenticate, getLoansByUser)
 router.delete("/:loanId/delete",[authenticate, authorize(["superAdmin"])], deleteLoad)
 
 export default router;
